446e7ab536ffca7df235bed2c7a1f42feb533dcf
mattjbr123 <mattjbr123@gmail.com>
2025-07-24 15:40:56 +0100

    First working version of gridded extract script

--- extract_grid.py
+++ extract_grid.py
@@ -12,9 +12,10 @@ import calendar
 import argparse
 import logging
 from dask.diagnostics import ProgressBar
+import pdb
 
 logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s',
+    format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
@@ -26,29 +27,35 @@ args = parser.parse_args()
 
 ensmem = args.ensmem # '01', '04', '06' or '15'
 outpath = args.outpath
+if not os.path.exists(outpath):
+    os.makedirs(outpath)
 
 # create time coords
 dayindex = pd.date_range("1/1/1981", "31/12/2079")
-years = dayindex.year.values
+allyears = dayindex.year.values
+allyears = [int(year) for year in allyears]
 doys = dayindex.dayofyear.values
+doys = [int(doy) for doy in doys]
 
 
 # read in CO2 data from cloud
 logging.info('Getting CO2 data from cloud')
-CO2datayr = pd.read_csv('s3://chess-scape-co2files/CHESS-SCAPE_RCP85_' + ensmem + '.csv', storage_options={'endpoint_url': "https://fdri-o.s3-ext.jc.rl.ac.uk", 'anon': True})
+CO2datayr = pd.read_csv('s3://chess-scape-co2files/CHESS-SCAPE_RCP85_' + ensmem + '.csv', 
+                        storage_options={'endpoint_url': "https://fdri-o.s3-ext.jc.rl.ac.uk", 'anon': True})
 CO2datayr = CO2datayr.set_index("YEAR")
 
 # put onto daily index (from yearly)
+logging.info("Converting to a daily coordinate (from yearly)")
 dayindex = pd.date_range("1/1/1981", "31/12/2079")
 CO2_daily = pd.DataFrame(index = dayindex, 
                          columns = ['CO2'])
-years = np.unique(dayindex.year.values)
+years = np.unique(allyears)
 for year in years:
-    CO2_daily.loc[str(year)] = CO2datayr.loc[str(year)]
+    CO2_daily.loc[str(year)] = CO2datayr.loc[year].values
 
 # load CHESS-SCAPE dataset from cloud
 # zarr v3 method
-logging.info('Loading cloud datasets')
+logging.info('Loading CHESS-SCAPE datasets')
 fs = s3fs.S3FileSystem(anon=True, asynchronous=True, endpoint_url="https://chess-scape-o.s3-ext.jc.rl.ac.uk")
 zstore_tmax = zarr.storage.FsspecStore(fs, path="ens" + ensmem + "-year100kmchunk/tmax_" + ensmem + "_year100km.zarr")
 zstore_tmin = zarr.storage.FsspecStore(fs, path="ens" + ensmem + "-year100kmchunk/tmin_" + ensmem + "_year100km.zarr")
@@ -71,20 +78,24 @@ ds = ds.set_coords(['lat','lon'])
 
 
 # select out & load coord block into RAM
-xslice = slice(200000,300000)
+logging.info("Extracting out chunk to RAM")
+xslice = slice(400000,500000)
 yslice = slice(400000,500000)
 tslice = slice("1981-01-01", "2079-12-31")
 with ProgressBar():
-    dschunk = ds.sel(x=xslice, y=yslice, time=tslice).compute()
+    dschunk = ds.sel(x=xslice, y=yslice).compute()
     
 # check there is some data there
-if np.all(np.isnan(dschunk)):
+logging.info("Checking NaNs...")
+if np.all(dschunk['rsds'][0,:,:].values == -1E20):
     logging.info('No data present in this chunk, exiting...')
     sys.exit()
 
 # convert to gregorian calendar from 360day
+logging.info("Move onto gregorian calendar (from 360-day)")
 dschunk_greg = dschunk.convert_calendar('gregorian', align_on='date', missing=np.nan)
 dschunk_greg = dschunk_greg.interpolate_na(dim='time', fill_value="extrapolate")
+dschunk_greg = dschunk_greg.sel(time=tslice)
 
 
 # convert units
@@ -104,25 +115,31 @@ dschunk_greg['pr'] = dschunk_greg['pr'] * 86400
 
 
 # loop over coords in chunk
+logging.info("Extracting out each gridpoint to csv")
 xs = dschunk_greg['x'].values
 ys = dschunk_greg['y'].values
-fileheading = ["DOY,RAD,MINTMP,MAXTMP,VP,WIND,RAIN,CO2"]
-#for x in xs:
-#    for y in ys:
-x = xs[0]
-y = ys[0]
 timelen = len(dschunk_greg['time'])
-dspoint = dschunk_greg.sel(x=x, y=y)        
-dataarray = np.zeros((timelen,9))
-dataarray[:,0] = years
-dataarray[:,1] = doys
-dataarray[:,2] = dspoint['rsds'].values
-dataarray[:,3] = dspoint['tasmin'].values
-dataarray[:,4] = dspoint['taxmax'].values
-dataarray[:,5] = dspoint['vp'].values
-dataarray[:,6] = dspoint['sfcWind'].values
-dataarray[:,7] = dspoint['pr'].values
-dataarray[:,8] = CO2_daily.values
-
-fname = 'chess-scape_1981-2079_' + str(ensmem) + '_' + str(x) + '_' + str(y) + '.csv'
-np.savetxt(fname, dataarray, fmt='%s', delimiter=',', header=fileheading)
+fileheading = "YEAR,DOY,RAD,MINTMP,MAXTMP,VP,WIND,RAIN,CO2"
+for x in xs:
+    for y in ys:
+        logging.info("Extracting out " + str(x) + "," + str(y))
+        dspoint = dschunk_greg.sel(x=x, y=y)
+        if dspoint['rsds'][0].values <= -1E10:
+            logging.info("No data in this coord: " + str(x) + "," + str(y))
+            continue
+        dataarray = np.zeros((timelen,9))
+        dataarray[:,0] = allyears
+        dataarray[:,1] = doys
+        dataarray[:,2] = dspoint['rsds'].values
+        dataarray[:,3] = dspoint['tasmin'].values
+        dataarray[:,4] = dspoint['tasmax'].values
+        dataarray[:,5] = dspoint['vp'].values
+        dataarray[:,6] = dspoint['sfcWind'].values
+        dataarray[:,7] = dspoint['pr'].values
+        dataarray[:,8] = CO2_daily['CO2'].values
+        
+        fname = os.path.join(outpath, 'chess-scape_1981-2079_' + str(ensmem) + '_' + str(x) + '_' + str(y) + '.csv')
+        logging.info("Saving to " + fname)
+        fmt_string = ['%.4d', '%1d', '%.5f', '%.2f', '%.2f', '%.5f', '%.2f', '%.5f', '%.4f']
+        np.savetxt(fname, dataarray, fmt=fmt_string, delimiter=',', header=fileheading)
+        logging.info("Saved")
